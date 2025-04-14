88108752747ed02db225aee6840866dc7f26f29a
ApoloApps <dev@composevisualeditor.com>
2025-04-14 19:13:38 +0200

    Modified release and artifact names 3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
         maven("https://repo.sellmair.io")
     }
 }
@@ -33,7 +33,7 @@ dependencyResolutionManagement {
         }
         maven("https://jitpack.io")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
         maven("https://repo.sellmair.io")
     }
 }
